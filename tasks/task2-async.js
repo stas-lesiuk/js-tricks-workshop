ae68d0c0f69d28f0d7ec8827392dcafc478f4514
Stanislav Lesiuk <stanislav.lesiuk@intive.com>
2018-01-26 14:10:33 +0100

    prepared simpler task2

--- tasks/task2-async.js
+++ tasks/task2-async.js
@@ -1,30 +1,21 @@
-firstPromise()
-  .then(function (firstPromiseResponse) {
-    // dataReceiver(data);
-    // anotherDataReceiver(data);
-    console.log('first promise here:', firstPromiseResponse);
-    return anotherPromise(firstPromiseResponse);
-  })
-  .then(function (secondPromiseResponse) {
-    console.log('second promise here:', secondPromiseResponse);
-  });
+var users = fakeUsersGet();
 
-function firstPromise() {
-  return new Promise(function (resolve, reject) {
-    setTimeout(function () {
-      resolve([1, 2, 3]);
-    }, 1000);
-  });
+function receiver(users) {
+  console.log('received users: ', users);
 }
 
-function anotherPromise(data) {
-  return new Promise(function (resolve, reject) {
-    try {
-      var x = y + z
+function fakeUsersGet() {
+  var users = [
+    {
+      name: 'Jon',
+      age: 15
+    },
+    {
+      name: 'Jack',
+      age: 22
     }
-    catch (error) {
-      reject(error);
-    }
-    resolve(data.length);
-  })
+  ];
+  return setTimeout(function () {
+    return users;
+  }, 1000);
 }
